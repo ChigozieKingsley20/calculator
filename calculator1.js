222aa6ba2fff67e8a2734a9741782256619674e0
Kingsley Chigozie <110520521+ChigozieKingsley20@users.noreply.github.com>
2022-11-12 04:03:44 +0400

    Add files via upload

---
+++ calculator1.js
@@ -0,0 +1,73 @@
+
+
+//arithmetic oparations
+function add(){
+ fat = parseInt(myform.fat.value);
+ fit = parseInt(myform.fit.value);
+ result = fat + fit;
+  }
+
+function subtract(){
+    fat = parseInt(myform.fat.value);
+    fit = parseInt(myform.fit.value);
+    result = fat - fit;
+  }   
+
+function multiply(){
+    fat = parseInt(myform.fat.value);
+    fit = parseInt(myform.fit.value);
+    result = fat * fit;
+  }   
+
+function divide(){
+    fat = parseInt(myform.fat.value);
+    fit = parseInt(myform.fit.value);
+    result = fat / fit;
+  }   
+
+function remainder(){
+ fat = parseInt(myform.fat.value);
+ fit = parseInt(myform.fit.value);
+ result = fat % fit;
+
+  } 
+
+
+//functions to display the arithmetic calculations
+  function calculate() {
+  if(  myform.result.value = result) {
+    result = fat % fit;
+  }
+else{
+}
+  }
+
+
+  function calculate() {
+    if(  myform.result.value = result) {
+      result = fat + fit;
+    }
+  else{
+  }
+    }
+
+
+  
+    
+  function calculate() {
+    if(  myform.result.value = result) {
+      result = fat * fit;
+    }
+  else{
+  }
+    }
+
+
+    
+  function calculate() {
+    if(  myform.result.value = result) {
+      result = fat / fit;
+    }
+  else{
+  }
+    }
